f1b19681fe62f622acfa31eb9a95085e348df3af
Aaron <69317200+Aaronphilip2003@users.noreply.github.com>
2022-02-27 10:46:54 +0530

    Create Solidity_Array.sol

---
+++ Solidity_Array.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract SimpleStorage{
+
+    uint256 public myNum;
+
+
+    struct People{
+        uint256 myFavNum;
+        string name;
+    }
+
+    People[] public people;
+
+    function store(uint256 a) public{
+        myNum=a;
+    }
+
+    function retrieve()public view returns(uint256){
+        return myNum;
+    }
+
+    function addPerson(uint256 a, string memory b) public{
+        people.push(People(a,b));
+    }
+
+
+}
