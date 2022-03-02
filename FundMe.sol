1935fd208f336aaeb7ac64eabd0d90ec160a61f7
Aaron <69317200+Aaronphilip2003@users.noreply.github.com>
2022-03-02 19:13:49 +0530

    Update FundMe.sol

--- FundMe.sol
+++ FundMe.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity >0.6.6 <0.9.0;
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
 
@@ -11,6 +13,19 @@ contract FundMe {
         AddressToNumber[msg.sender]+=msg.value;
     }
 
+    function getVersion() public view returns(uint256){
+        AggregatorV3Interface priceFeed=AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331);
+        return priceFeed.version();      
+
+    }
+
+    function getPrice() public view returns(uint256){
+        AggregatorV3Interface priceFeed=AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331);
+        (,int price,,,) = priceFeed.latestRoundData();
+
+        return uint256(price);       
+
+    }
 
 
 }
